4382c19395182ebaf3e66b9300813ca0a6e27106
omrishefi <omrishefi@google.com>
2025-07-27 13:59:47 +0300

    Add masked mean embeddings

--- neuralhydrology/modelzoo/mean_embedding_forecast_lstm.py
+++ neuralhydrology/modelzoo/mean_embedding_forecast_lstm.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Iterable
 
 import math
 import torch
@@ -15,10 +15,10 @@ from neuralhydrology.datautils import mean_embedding_forecast_lstm_datautils
 class MeanEmbeddingForecastLSTM(BaseModel):
     """A forecasting model using stacked LSTMs for hindcast and forecast.
 
-    This is a forecasting model that uses two stacked sequential (LSTM) models to handle 
+    This is a forecasting model that uses two stacked sequential (LSTM) models to handle
     hindcast vs. forecast. The config parameter ``forecast_overlap`` deterimines the temporal
     overlap of the hindcast and forecast LSTMs.
-        
+
     Outputs from the hindcast LSTM are concatenated to the input sequences to the forecast
     LSTM and shifted in time by the forecast horizon. This causes a lag between the latest
     hindcast data and the newest forecast time point, meaning that forecasts do not get
@@ -33,14 +33,21 @@ class MeanEmbeddingForecastLSTM(BaseModel):
     ----------
     cfg : Config
         The run configuration.
-        
+
     Raises
     ------
     ValueError if `predict_last_n` is longer than the total forecast sequence (overlap + lead time).
     ValueError if `seq_length` is shorter than `forecast_overlap`.
     """
+
     # Specify submodules of the model that can later be used for finetuning. Names must match class attributes.
-    module_parts = ['hindcast_embedding_net', 'forecast_embedding_net', 'forecast_lstm', 'hindcast_lstm', 'head']
+    module_parts = [
+        "hindcast_embedding_net",
+        "forecast_embedding_net",
+        "forecast_lstm",
+        "hindcast_lstm",
+        "head",
+    ]
 
     def __init__(self, cfg: Config):
         super(MeanEmbeddingForecastLSTM, self).__init__(cfg=cfg)
@@ -103,13 +110,15 @@ class MeanEmbeddingForecastLSTM(BaseModel):
         # TODO (future) :: Models assume that all lead times are present up to the longest `lead_time`.
         # ForecastBaseDataset does not require this assumption.
         if cfg.predict_last_n > self.lead_time + self.overlap:
-            raise ValueError('`predict_last_n` must not be larger than the length of the forecast sequence.')
+            raise ValueError(
+                "`predict_last_n` must not be larger than the length of the forecast sequence."
+            )
         if cfg.seq_length < self.overlap:
-            raise ValueError('`seq_length` must be larger than `forecast_overlap`.')
+            raise ValueError("`seq_length` must be larger than `forecast_overlap`.")
 
         # Input embedding layers.
-        self.forecast_embedding_net = InputLayer(cfg=cfg, embedding_type='forecast')
-        self.hindcast_embedding_net = InputLayer(cfg=cfg, embedding_type='hindcast')
+        self.forecast_embedding_net = InputLayer(cfg=cfg, embedding_type="forecast")
+        self.hindcast_embedding_net = InputLayer(cfg=cfg, embedding_type="hindcast")
 
         # Time series layers.
         self.hindcast_lstm = nn.LSTM(
@@ -118,13 +127,15 @@ class MeanEmbeddingForecastLSTM(BaseModel):
             bidirectional=cfg.bidirectional_stacked_forecast_lstm,
         )
 
-        forecast_input_size = self.forecast_embedding_net.output_size + self.hindcast_lstm.hidden_size
+        forecast_input_size = (
+            self.forecast_embedding_net.output_size + self.hindcast_lstm.hidden_size
+        )
         if self.cfg.bidirectional_stacked_forecast_lstm:
             forecast_input_size += self.hindcast_lstm.hidden_size
         self.forecast_lstm = nn.LSTM(
             input_size=forecast_input_size,
             hidden_size=cfg.hidden_size,
-            batch_first=True
+            batch_first=True,
         )
 
         self.dropout = nn.Dropout(p=cfg.output_dropout)
@@ -135,12 +146,18 @@ class MeanEmbeddingForecastLSTM(BaseModel):
     def _reset_parameters(self):
         """Special initialization of certain model weights."""
         if self.cfg.initial_forget_bias is not None:
-            self.hindcast_lstm.bias_hh_l0.data[self.cfg.hidden_size:2 * self.cfg.hidden_size] = self.cfg.initial_forget_bias
-            self.forecast_lstm.bias_hh_l0.data[self.cfg.hidden_size:2 * self.cfg.hidden_size] = self.cfg.initial_forget_bias
+            self.hindcast_lstm.bias_hh_l0.data[
+                self.cfg.hidden_size : 2 * self.cfg.hidden_size
+            ] = self.cfg.initial_forget_bias
+            self.forecast_lstm.bias_hh_l0.data[
+                self.cfg.hidden_size : 2 * self.cfg.hidden_size
+            ] = self.cfg.initial_forget_bias
 
-    def _append_static_embeddings(self, embeddings: torch.Tensor, *, static_embeddings: torch.Tensor) -> torch.Tensor:
+    def _append_static_embeddings(
+        self, embeddings: torch.Tensor, *, static_embeddings: torch.Tensor
+    ) -> torch.Tensor:
         """Pad the embedding tensor with the embedding of static attributes.
- 
+
         Parameters
         ----------
         embeddings : torch.Tensor
@@ -158,7 +175,7 @@ class MeanEmbeddingForecastLSTM(BaseModel):
 
     def _add_nan_padding(self, embeddings: torch.Tensor) -> torch.Tensor:
         """Pad the embedding tensor with nan value.
- 
+
         Parameters
         ----------
         embeddings : torch.Tensor
@@ -176,10 +193,18 @@ class MeanEmbeddingForecastLSTM(BaseModel):
         nan_padding_length = self.seq_length + self.lead_time - embeddings.shape[1]
         # Dimension 2 is the length of embedding vector.
         embedding_size = embeddings.shape[2]
-        nan_padding = torch.full((batch_size, nan_padding_length, embedding_size), math.nan)
+        nan_padding = torch.full(
+            (batch_size, nan_padding_length, embedding_size), math.nan
+        )
         return torch.cat([embeddings, nan_padding], dim=1)
 
-    def forward(self, data: dict[str, torch.Tensor | dict[str, torch.Tensor]]) -> Dict[str, torch.Tensor]:
+    def _masked_mean_embedding(self, data: Iterable[torch.Tensor]) -> torch.Tensor:
+        merged = torch.cat([e.unsqueeze(-1) for e in data], dim=-1)
+        return torch.nanmean(merged, dim=-1)
+
+    def forward(
+        self, data: dict[str, torch.Tensor | dict[str, torch.Tensor]]
+    ) -> Dict[str, torch.Tensor]:
         """Perform a forward pass on the StackedForecastLSTM model.
 
         Parameters
@@ -198,11 +223,15 @@ class MeanEmbeddingForecastLSTM(BaseModel):
         )
         static_embeddings = self.static_attributes_fc(forward_data.static_attributes)
 
-        cpc_input_concat = self._append_static_embeddings(forward_data.cpc_data, static_embeddings=static_embeddings)
+        cpc_input_concat = self._append_static_embeddings(
+            forward_data.cpc_data, static_embeddings=static_embeddings
+        )
         cpc_embeddings = self.cpc_input_fc(cpc_input_concat)
         cpc_embedding_with_nan = self._add_nan_padding(cpc_embeddings)
 
-        imerg_input_concat = self._append_static_embeddings(forward_data.imerg_data, static_embeddings=static_embeddings)
+        imerg_input_concat = self._append_static_embeddings(
+            forward_data.imerg_data, static_embeddings=static_embeddings
+        )
         imerg_embeddings = self.imerg_input_fc(imerg_input_concat)
         imerg_embedding_with_nan = self._add_nan_padding(imerg_embeddings)
 
@@ -216,8 +245,19 @@ class MeanEmbeddingForecastLSTM(BaseModel):
         )
         graphcast_embeddings = self.graphcast_input_fc(graphcast_input_concat)
 
-        # Masked mean hindcast (cpc, imerg, hres, graphcast)
-        # Masked mean forcast (hres, graphcast)
+        hindcast_mean_embedding = self._masked_mean_embedding(
+            [
+                cpc_embedding_with_nan,
+                imerg_embedding_with_nan,
+                hres_embeddings,
+                graphcast_embeddings,
+            ]
+        )
+
+        forecast_mean_embedding = self._masked_mean_embedding(
+            [hres_embeddings, graphcast_embeddings]
+        )
+
         # LSTM hindcast (masked mean hindcast, static)
         # LSTM forecast (lstm hindcast, masked mean forcast, static)
         # Head (lstm forecast)
@@ -234,7 +274,9 @@ class MeanEmbeddingForecastLSTM(BaseModel):
         # forecast, _ = self.forecast_lstm(forecast_inputs)
 
         # Run head.
-        result = (torch.rand(data['x_s'].shape[0], self.overlap + self.lead_time, 128) * 2) - 1
+        result = (
+            torch.rand(data["x_s"].shape[0], self.overlap + self.lead_time, 128) * 2
+        ) - 1
         # transposed = forecast.transpose(0, 1)
         return self.head(self.dropout(result))
         # return result
