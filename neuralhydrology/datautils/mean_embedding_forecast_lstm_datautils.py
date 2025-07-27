4ae59bf2f04eba77610f9f11935a08691d250bbe
amitmarkel <amarkel@google.com>
2025-07-27 12:08:38 +0300

    Add forecast_inputs to ForwardData.

--- neuralhydrology/datautils/mean_embedding_forecast_lstm_datautils.py
+++ neuralhydrology/datautils/mean_embedding_forecast_lstm_datautils.py
@@ -58,8 +58,22 @@ class ForwardData:
             static_attributes=data["x_s"],
             cpc_data=data["x_d_hindcast"]["cpc_precipitation"],
             imerg_data=data["x_d_hindcast"]["imerg_precipitation"],
+            forecast_inputs=_gather_forecast_inputs(data),
         )
 
     static_attributes: torch.Tensor
     cpc_data: torch.Tensor
     imerg_data: torch.Tensor
+    forecast_inputs: torch.Tensor
+
+
+def _gather_forecast_inputs(
+    data: dict[str, torch.Tensor | dict[str, torch.Tensor]],
+) -> torch.Tensor:
+    forecast_input_tensors = [
+        data["x_d_forecast"]["graphcast_temperature_2m"],
+        data["x_d_forecast"]["graphcast_total_precipitation"],
+        data["x_d_forecast"]["graphcast_u_component_of_wind_10m"],
+        data["x_d_forecast"]["graphcast_v_component_of_wind_10m"],
+    ]
+    return torch.cat(forecast_input_tensors, dim=-1)
