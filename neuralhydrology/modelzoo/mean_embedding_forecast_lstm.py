ad9cd00ff418483d99493249768eb8aa34d30e2f
amitmarkel <amarkel@google.com>
2025-07-28 13:15:42 +0300

    Use the library's CMAL Loss function instead of NSE loss and thus y_hat is unneeded.

--- neuralhydrology/modelzoo/mean_embedding_forecast_lstm.py
+++ neuralhydrology/modelzoo/mean_embedding_forecast_lstm.py
@@ -261,15 +261,4 @@ class MeanEmbeddingForecastLSTM(BaseModel):
 
         predictions = self.head(self.dropout(forecast))
 
-        # create deterministic "best guess" prediction:
-        # - weighted avg of location parameters (mu),
-        # - using the model's learnt mixture weights (pi).
-        # Mu's values are the mean for each distribution.
-        # Pi's values are likewise for the model's confidence for each dist and should sum to 1.
-        # So, multiply those element-wise to scale each location mu by factor pi.
-        # NOTE: sum on the last dim because that's the distribution feature values.
-        # NOTE: keep the last dim to preserve shape. E.g. if x=[[1,2,3],[4,5,6]] so sum(x, dim=1)=[6,15] but with keepdim=True it's [[6],[15]]. We need to retain (batch_size, sequence_length, n_distributions) resulting in (batch_size, sequence_length, 1) and not (batch_size, sequence_length,).
-        # For example, suppose n_distributions=3, mu=[10.0, 15.0, 12.0] and [0.1, 0.7, 0.2]. It means the model is most confident in the second dist (most importance in pi). So 0.1*10.0+0.7*15.0+0.2*12.0=1.0+10.5+2.4=13.9.
-        y_hat = torch.sum(predictions["pi"] * predictions["mu"], dim=-1, keepdim=True)
-
-        return {"y_hat": y_hat} | predictions
+        return predictions
