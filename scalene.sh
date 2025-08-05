a0570a56b67384c31afc1fb86510f93266eb54bb
amitmarkel <amarkel@google.com>
2025-08-06 00:06:07 +0300

    Add scalene.sh profiling.

---
+++ scalene.sh
@@ -0,0 +1,2 @@
+echo Be patient after done running.
+scalene --profile-all --stacks --web $HOME/neuralhydrology/neuralhydrology/nh_run.py --- train --config-file $HOME/neuralhydrology/config/multimet_mean_embedding_forecast_lstm.yml
