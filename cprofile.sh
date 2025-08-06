1372f858073eb9d7e78226988cd171d2257fda09
amitmarkel <amarkel@google.com>
2025-08-07 00:17:46 +0300

    Move profilers to ./profilers/

--- cprofile.sh
+++
@@ -1,3 +0,0 @@
-rm /tmp/profile.pstats
-python3 -m cProfile -o /tmp/profile.pstats $HOME/neuralhydrology/neuralhydrology/nh_run.py train --config-file config/multimet_mean_embedding_forecast_lstm.yml
-gprof2dot -f pstats /tmp/profile.pstats | dot -Tpng -o /tmp/profile.png
--- memray.sh
+++
@@ -1,2 +0,0 @@
-memray run --aggregate --native neuralhydrology/nh_run.py \
-        train --config-file config/multimet_mean_embedding_forecast_lstm.yml
