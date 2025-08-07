363b4409bd7dfa12baab9384c8798aea17890162
amitmarkel <amarkel@google.com>
2025-08-07 11:30:55 +0300

    Fix scalene tmp output rm and set output to tmp and open it.

--- profilers/scalene_profiler.sh
+++ profilers/scalene_profiler.sh
@@ -1,3 +1,4 @@
+rm -f /tmp/scalene_output.html
 echo Be patient after done running.
-echo Scalene opens in a web browser
-scalene --profile-all --stacks --web $HOME/neuralhydrology/neuralhydrology/nh_run.py --- train --config-file $HOME/neuralhydrology/config/multimet_mean_embedding_forecast_lstm.yml
+scalene --profile-all --stacks --web --outfile /tmp/scalene_output.html $HOME/neuralhydrology/neuralhydrology/nh_run.py --- train --config-file $HOME/neuralhydrology/config/multimet_mean_embedding_forecast_lstm.yml
+open /tmp/scalene_output.html
