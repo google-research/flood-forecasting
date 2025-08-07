f9459f956ec344772f2cbd7821af248b9f4a3afc
amitmarkel <amarkel@google.com>
2025-08-07 11:23:49 +0300

    Fix tmp file removal and open image result automatically.

--- profilers/cprofile.sh
+++ profilers/cprofile.sh
@@ -1,3 +1,4 @@
-rm /tmp/profile.pstats
+rm -f /tmp/profile.pstats /tmp/profile.png
 python3 -m cProfile -o /tmp/profile.pstats $HOME/neuralhydrology/neuralhydrology/nh_run.py train --config-file $HOME/neuralhydrology/config/multimet_mean_embedding_forecast_lstm.yml
 gprof2dot -f pstats /tmp/profile.pstats | dot -Tpng -o /tmp/profile.png
+open /tmp/profile.png
