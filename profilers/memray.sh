1a8b5b314688a746931b2178e42640f10f278194
amitmarkel <amarkel@google.com>
2025-08-07 11:16:47 +0300

    Setup memray outputs at /tmp and open the html automatically.

--- profilers/memray.sh
+++ profilers/memray.sh
@@ -1,2 +1,10 @@
-memray run --aggregate --native ~/neuralhydrology/neuralhydrology/nh_run.py \
+rm -f /tmp/memray_output.bin /tmp/memray_flamegraph.html
+
+echo Profiling...
+memray run --aggregate --native -o /tmp/memray_output.bin ~/neuralhydrology/neuralhydrology/nh_run.py \
         train --config-file ~/neuralhydrology/config/multimet_mean_embedding_forecast_lstm.yml
+
+echo Analysing...
+memray flamegraph -o /tmp/memray_flamegraph.html /tmp/memray_output.bin
+
+open /tmp/memray_flamegraph.html
