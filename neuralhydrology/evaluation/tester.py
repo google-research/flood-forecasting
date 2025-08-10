abaa52325bb5552befca646a575947d23219e5b5
amitmarkel <amarkel@google.com>
2025-08-10 17:22:08 +0300

    fix line

--- neuralhydrology/evaluation/tester.py
+++ neuralhydrology/evaluation/tester.py
@@ -554,6 +554,7 @@ class UncertaintyTester(BaseTester):
         LOGGER.debug('getting model predictions (sample)')
         predictions = model.sample(data, self.cfg.n_samples)
         LOGGER.debug('getting model eval')
+        model.eval()
         return predictions, {k: v.item() for k, v in all_losses.items()}
 
     def _subset_targets(self,
