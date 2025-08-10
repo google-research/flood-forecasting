e87e66d7ea6b642a30f95c8aab961225761c61f6
amitmarkel <amarkel@google.com>
2025-08-10 16:45:41 +0300

    Cache nan padding tensors.

--- neuralhydrology/modelzoo/mean_embedding_forecast_lstm.py
+++ neuralhydrology/modelzoo/mean_embedding_forecast_lstm.py
@@ -163,8 +163,23 @@ class MeanEmbeddingForecastLSTM(BaseModel):
     ) -> torch.Tensor:
         return static.unsqueeze(1).repeat(1, time_length, 1)
 
+    @functools.cache
+    def _make_nan_padding_cached(
+        self,
+        batch_size: int,
+        nan_padding_length: int,
+        embedding_size: int,
+        device: str,
+    ) -> torch.Tensor:
+        return torch.full(
+            (batch_size, nan_padding_length, embedding_size), np.nan, device=device
+        )
+
     def _clear_forward_caches(self):
+        # Clear since static attributes change every forward cycle:
         self._make_static_attributes_repeated_cached.cache_clear()
+
+        # No need to clear self._make_nan_padding_cached, it's always constant.
 
     def _append_static_attributes(
         self, embedding: torch.Tensor, static_attributes: torch.Tensor
@@ -185,10 +200,8 @@ class MeanEmbeddingForecastLSTM(BaseModel):
         nan_padding_length = self.seq_length + self.lead_time - embedding.shape[1]
         # Dimension 2 is the length of embedding vector.
         embedding_size = embedding.shape[2]
-        nan_padding = torch.full(
-            (batch_size, nan_padding_length, embedding_size),
-            np.nan,
-            device=embedding.device,
+        nan_padding = self._make_nan_padding_cached(
+            batch_size, nan_padding_length, embedding_size, embedding.device
         )
         return torch.cat([embedding, nan_padding], dim=1)
 
