63a57d7671b07113609c842942f7a5ca57bc554e
amitmarkel <amarkel@google.com>
2025-09-14 10:54:02 +0000

    Optimize date extract.

--- neuralhydrology/datasetzoo/forecast_basedataset.py
+++ neuralhydrology/datasetzoo/forecast_basedataset.py
@@ -230,20 +230,17 @@ class ForecastDataset(BaseDataset):
         if item % 1 != 0:
             raise ValueError(f'Requested index {item} is not an integer.')
 
+        def _calc_date_range() -> range:
+            date = self._sample_index[item]["date"]
+            duration = self._seq_length - 1
+            if not self._lead_times:
+                return range(date - duration, date + 1)
+            end = date + self._lead_times[-1]
+            return range(end - duration, end + 1)
+
         # Extract sample from `self._dataset`.
-        def _extract_dates(item: int) -> np.ndarray:
-            hindcast_date_indexes = range(
-                self._sample_index[item]['date']+1-self._seq_length,
-                self._sample_index[item]['date']+1
-            )
-            dates = self._extract_dataset(self._dataset, 'dataset', 'date', {'date': hindcast_date_indexes})
-            if self._lead_times:
-                forecast_dates = [
-                    dates[-1] + np.timedelta64(i, 'D')
-                    for i in self._lead_times
-                ]
-                dates = np.concatenate([dates, forecast_dates])[-self._seq_length:]
-            return dates
+        def _extract_dates() -> np.ndarray:
+            return self._extract_dataset(self._dataset, 'dataset', 'date', {'date': _calc_date_range()})
 
         def _extract_statics(feature: str, item: int) -> np.ndarray:
             return self._extract_dataset(self._dataset, 'dataset', feature, {'basin': self._sample_index[item]['basin']})
@@ -268,12 +265,10 @@ class ForecastDataset(BaseDataset):
 
         def _extract_targets(feature: str, item: int) -> np.ndarray:
             dim_indexes = self._sample_index[item].copy()
-            end = dim_indexes["date"] + self._lead_times[-1]  # e.g. 1000 (date) + 7 (lead time)
-            start = end - (self._seq_length - 1)  # e.g. 365 window starting on 1007 starts on 643
-            dim_indexes["date"] = range(start, end + 1)  # include the last day
+            dim_indexes["date"] = _calc_date_range(lead=True)
             return self._extract_dataset(self._dataset, "dataset", feature, dim_indexes)
 
-        sample = {'date': _extract_dates(item)}
+        sample = {'date': _extract_dates()}
         # TODO (future) :: Suggest remove outer keys and use only feature names. Major change required.
         sample['x_s'] = np.stack([_extract_statics(feature, item) for feature in self._static_features], -1)
         sample['x_d_hindcast'] = {feature: _extract_hindcasts(feature, item) for feature in self._hindcast_features}
