ee678e335dd08f807827b8907d584c02ade19ebb
omrishefi <omrishefi@google.com>
2025-09-16 15:22:55 +0300

    Add Apache 2.0 license header for all source files

--- profilers/cprofile.sh
+++ profilers/cprofile.sh
@@ -1,3 +1,17 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 rm -f /tmp/profile.pstats /tmp/profile.png
 python3 -m cProfile -o /tmp/profile.pstats $HOME/neuralhydrology/neuralhydrology/nh_run.py train --config-file $HOME/neuralhydrology/config/multimet_mean_embedding_forecast_lstm.yml
 gprof2dot -f pstats /tmp/profile.pstats | dot -Tpng -o /tmp/profile.png
