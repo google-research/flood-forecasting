008fb2a81caf9b3dd649e2a9072379bcf509109d
amitmarkel <amarkel@google.com>
2025-11-09 20:08:50 +0200

    Refine comment

--- googlehydrology/utils/samplingutils.py
+++ googlehydrology/utils/samplingutils.py
@@ -161,7 +161,7 @@ def _sample_asymmetric_laplacians(
 ) -> torch.Tensor:
     # The ids are used for location-specific resampling for 'truncation' in '_handle_negative_values'
     m_sub_ids = m_sub[ids]
-    prob = torch.rand_like(m_sub_ids)  # sample uniformly between zero and 1
+    prob = torch.rand_like(m_sub_ids)  # sample uniformly in [0,1)
     t_sub_ids = t_sub[ids]
     t_sub_ids_c = 1 - t_sub_ids
     b_sub_ids = b_sub[ids]
