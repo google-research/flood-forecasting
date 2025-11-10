14fb3e2386aadb88e0e63f118ae55eade6d96acd
amitmarkel <amarkel@google.com>
2025-11-10 12:10:55 +0200

    Add scalene package check and installation at the script top.

--- tools/profilers/scalene_profiler.sh
+++ tools/profilers/scalene_profiler.sh
@@ -12,6 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -o pipefail
+
+(conda list | grep scalene > /dev/null) || (conda install scalene -c conda-forge)
+(conda list | grep scalene > /dev/null) || (echo "scalene is required" && exit -1)
+
 rm -f /tmp/scalene_output.html
 echo Be patient after done running.
 scalene --profile-all --stacks --web --outfile /tmp/scalene_output.html $HOME/flood-forecasting/googlehydrology/run.py --- train --config-file $HOME/flood-forecasting/config/multimet_mean_embedding_forecast_lstm.yml
